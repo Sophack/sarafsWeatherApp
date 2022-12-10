f4f04dd424ce97498cd359c07ef4e568403078b0
Saraf Nawar <s.999895831@gmail.com>
2022-12-09 23:29:32 -0500

    nothing is working

--- script.js
+++ script.js
@@ -1,76 +1,52 @@
-//function for getting the current date 
-
-
-// document.getElementById("date").innerHTML = myDate.toLocaleDateString();
-//Getting and displaying the text for the upcoming five days of the week
-var now = new Date();
-var daysOfYear = [];
-for (var d = new Date(2012, 0, 1); d <= now; d.setDate(d.getDate() + 1)) {
-    daysOfYear.push(new Date(d));
-}
-
-    document.getElementById("img").src = "http://openweathermap.org/img/w/"+
-data.list[0].weather[0].icon
-+".png";
-
 let weather = {
-    apiKey : "5d4f571990203e94a636bb9b71ef6ae5",
+    apiKey: "5d4f571990203e94a636bb9b71ef6ae5",
     fetchWeather: function (city) {
       fetch(
-        "https://api.openweathermap.org/data/2.5/weather?q=" +
-          city +
-          "&units=metric&appid=" +
-          this.apiKey
+        "https://api.openweathermap.org/data/2.5/forecast?cnt=5&q=" 
+        + city
+        +"&units=metric&appid="
+        + this.apiKey 
       )
-        .then((response) => {
-          if (!response.ok) {
-            alert("No weather found.");
-            throw new Error("No weather found.");
-          }
-          return response.json();
-        })
-        .then((data) => this.displayWeather(data));
-    }}
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data))
+    },
+    displayWeather: function (data) {
+    //variables and where they will be extracted from
+      const { name } = data;
+      const { icon, description } = data.weather[0];
+      const { temp, humidity } = data.main;
+      const { speed } = data.wind;
+      console.log(name,icon,temp,humidity,speed,description);
+    }
+};
 
-// function getInfo () {
 
-//     //variabls for user input & city name 
 
-//     var newName = document.getElementById("cityInput"), //passing the ID for the input box
-//     var cityName = document.getElementById("cityName"), //target ID for the city input box 
-
-//     //watever the user enters into the field will be used & displayed 
-
-//     cityName.innerHTML = "" + newName.value + ""  ;
-
-//     //fetching the 5 day forecast data by adding the user input for city in metric 
-//     //passing the API link with API key retrieved from open weather map site  
-    
-//     fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&units=metric&appid=5d4f571990203e94a636bb9b71ef6ae5')
-//    //promises
-//     .then(response => response.json())
-//     .then(data => {
-//             const { name } = data;
-//             const { icon, description } = data.weather[1];
-//             const { temp, humidity } = data.main;
-//             const { speed } = data.wind;
-//             document.querySelector(".city").innerText = "Weather in " + name;
-//             document.querySelector(".icon").src =
-//               "https://openweathermap.org/img/wn/" + icon + ".png";
-//             document.querySelector(".description").innerText = description;
-//             document.querySelector(".temp").innerText = temp + "°C";
-//             document.querySelector(".humidity").innerText =
-//               "Humidity: " + humidity + "%";
-//             document.querySelector(".wind").innerText =
-//               "Wind speed: " + speed + " km/h";
-//             document.querySelector(".weather").classList.remove("loading");
-//             document.body.style.backgroundImage =
-//               "url('https://source.unsplash.com/1600x900/?" + name + "')";
+      //       document.querySelector("#cityName").innerText = "Weather in " + name;
+//       document.querySelector(".icon").src =
+//         "https://openweathermap.org/img/wn/" + icon + ".png";
+//       document.querySelector(".temp").innerText = temp + "°C";
+//       document.querySelector(".humidity").innerText =
+//         "Humidity: " + humidity + "%";
+//       document.querySelector(".wind").innerText =
+//         "Wind speed: " + speed + " km/h";
+//       document.querySelector(".weather").classList.remove("loading");
+//     },
+//     search: function () {
+//       this.fetchWeather(document.querySelector(".search-bar").value);
+//     },
+//   };
+  
+//   document.querySelector(".button").addEventListener("click", function () {
+//     weather.search();
+//   });
+  
+//   document
+//     .querySelector(".search-bar")
+//     .addEventListener("keyup", function (event) {
+//       if (event.key == "Enter") {
+//         weather.search();
+//       }
 //     });
-
-
-
-
-
-
-
+  
+//   weather.fetchWeather("Denver");
