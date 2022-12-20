992d5661e61405c74f31b7a8c62efe6178216d1e
Saraf Nawar <s.999895831@gmail.com>
2022-12-20 14:50:25 -0500

    2 events for search button

--- script.js
+++ script.js
@@ -1,106 +1,91 @@
-//this selects the user input from the input type and lets us search by city name 
-  
-    let searchCity = document.querySelector("#cityInput");
+//below selects the user input from the document's input type and lets us search by city name
 
-    let apiKey =  "80de82d1907b221f875a400ad16653d2";
-    
-   //we can call the function below to execute user search input 
+let searchCity = document.querySelector("#cityInput");
 
-   function fetchWeather (city) {
-    // const data = should be the api data 
-      fetch(
-        "https://api.openweathermap.org/data/2.5/forecast?cnt=5&q=" 
-        + city
-        +"&units=metric&appid="
-        + apiKey 
-      )
-        .then((response) => {
-          if (response.ok) {
-            return response.json();
-          }else{
-            throw new Error("Network problems. Unable to get weather"); 
-          }})
-            .then((data) => 
-            displayWeather(data) )
-            
-    } 
-  
-  //parsing: specific component inside of compiler 
-  //compiler: translates inputs & outputs 
-  //input (string) best way to handle it 
+let apiKey = "80de82d1907b221f875a400ad16653d2";
 
+//we can call the function below to execute user search input
 
-//assign data to be extracted from the API 
+function fetchWeather(city) {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/forecast?cnt=5&q=" +
+      city +
+      "&units=metric&appid=" +
+      apiKey
+  )
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Network problems. Unable to get weather");
+      }
+    })
+    .then((data) => displayWeather(data));
+}
 
-  //  let data = 
-    //function to take in data and display weather
-  function displayWeather (data) {
-    //variables and where they will be extracted from
-      const  date  = new Date();
-      const  name  = data.city.name;
-      const  temp = data.list[0].main.temp;
-      const  speed  = data.list[0].wind.speed;
-      const humidity = data.list[0].main.humidity;
-      const icon = data.list[0].weather[0].icon;
- 
- document.querySelector("#cityName").innerText = name;
- document.querySelector(".icon").src =
-   "https://openweathermap.org/img/wn/" + icon + "@2x.png";
- document.querySelector(".temp").innerText = "Temp: " + temp + "°C";
- document.querySelector(".humidity").innerText =
-   "Humidity: " + humidity + "%";
- document.querySelector(".wind").innerText =
-   "Wind speed: " + speed + " km/h";
-
- document.querySelector("#date").innerText = (date.toDateString());
-
- 
-      //should log all the 5 variables into console 
-      console.log(date,name,icon,temp,humidity,speed);
-
-      
-
-  }
-
-
-   
-     //
-  function handleSearch(){ 
-    fetchWeather(searchCity.value);
-    displayWeather(fetchWeather);
-
-    
-  }
-  localStorage.setItem(searchCity, "value")
-
-   document.querySelector(".srchButton")
-   .addEventListener("click", handleSearch);
-
-
-fetchWeather("Atlanta");
-
-
-
-
-// document.querySelector(".search-bar").addEventListener("keyup", function (event) {
-// }
-  
-     //currently console.logs the user input 
-     //create function to have it show up on app
-     //create function to display under forecast 
-     document.querySelector(".icon").src =
-     "https://openweathermap.org/img/wn/" + icon + "@2x.png";
-
-function weatherForecast (){
-  const  weatherDays= [date,temp,speed]
-  const  date  = new Date();
-  const  temp = data.list[0].main.temp;
-  const  speed  = data.list[0].wind.speed;
+//function to pass data and display the weather
+function displayWeather(data) {
+  //setting values for each
+  const date = new Date();
+  const name = data.city.name;
+  const temp = data.list[0].main.temp;
+  const speed = data.list[0].wind.speed;
   const humidity = data.list[0].main.humidity;
   const icon = data.list[0].weather[0].icon;
 
-    for(i=0;i<5;i++){
-      document.querySelector(".date").innerText = date + temp + speed + icon + humidity};
-    
-  
-console.log(weatherForecast)};
+  //setting up where to retreive the information from the HTML
+  document.querySelector("#cityName").innerText = name;
+  document.querySelector(".icon").src =
+    "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+  document.querySelector(".temp").innerText = "Temp: " + temp + "°C";
+  document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+  document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
+
+  document.querySelector("#date").innerText = date.toDateString();
+
+  //log all the 5 variables into console
+  console.log(date, name, icon, temp, humidity, speed);
+}
+
+//function to display the info for the city
+
+function handleSearch() {
+  //passing user input with fetchWeather funciton
+  fetchWeather(searchCity.value);
+  //calling displayWeather function by passing it with fetchWeather function
+  displayWeather(fetchWeather);
+}
+
+//button user will click to initiate the search
+document.querySelector(".srchButton").addEventListener("click", handleSearch);
+
+//passing the city with the fetchWeather function so by default the homepage will show Atlanta's weather
+fetchWeather("Atlanta");
+
+function weatherForecast() {
+  setData = (displayWeather) => {
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+    const weatherDays = [date, temp, speed];
+    const date = new Date();
+    const temp = data.list[0].main.temp;
+    const speed = data.list[0].wind.speed;
+    const humidity = data.list[0].main.humidity;
+    const icon = data.list[0].weather[0].icon;
+  };
+
+  //for loop will be executed until i is at 5 with increment of 1
+  for (let i = 0; i < 5; i++) {
+    document.querySelector("#date").innerText;
+    console.log(date[i]);
+  }
+
+  // console.log(weatherForecast);
+
+  //storing user input
+  localStorage.setItem(searchCity, "value");
+
+  //retreiving previous user input
+  localStorage.getItem(searchCity, "value");
+}
