c313eebdf2bb5fdf21d527d664255ae324011a40
Saraf Nawar <s.999895831@gmail.com>
2022-12-20 22:24:56 -0500

    for loop working

--- script.js
+++ script.js
@@ -4,13 +4,19 @@ let searchCity = document.querySelector("#cityInput");
 
 let apiKey = "80de82d1907b221f875a400ad16653d2";
 
-let cityName = document.querySelector("#cityName")
+//container
+let cityName;
+let temp;
+let speed;
+let humidity;
+let icon;
 
 //we can call the function below to execute user search input
 
 function fetchWeather(city) {
+  //fetch current data instead of forecast
   fetch(
-    "https://api.openweathermap.org/data/2.5/forecast?cnt=5&q=" +
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
       city +
       "&units=metric&appid=" +
       apiKey
@@ -22,33 +28,38 @@ function fetchWeather(city) {
         throw new Error("Network problems. Unable to get weather");
       }
     })
-    .then((data) => displayWeather(data));
+    .then((data) => {
+      displayWeather(data);
+    });
 }
 
 //function to pass data and display the weather
+//to populate the information from the API call
 function displayWeather(data) {
+  console.log(data);
   //setting values for each
   const date = new Date();
-  const name = data.city.name;
-  const temp = data.list[0].main.temp;
-  const speed = data.list[0].wind.speed;
-  const humidity = data.list[0].main.humidity;
-  const icon = data.list[0].weather[0].icon;
+  const name = data.name;
+  const temp = data.main.temp;
+  const speed = data.wind.speed;
+  const humidity = data.main.humidity;
+  const icon = data.weather[0].icon;
 
   //setting up where to retreive the information from the HTML
-  cityName.innerText = name;
+  document.querySelector("#cityName").innerText = name;
   document.querySelector(".icon").src =
     "https://openweathermap.org/img/wn/" + icon + "@2x.png";
-
   document.querySelector(".temp").innerText = "Temp: " + temp + "°C";
   document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
   document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
 
   document.querySelector("#date").innerText = date.toDateString();
 
-  //log all the 5 variables into console
-  console.log(date, name, icon, temp, humidity, speed);
+  weatherForecast(name);
 }
+
+// //log all the 5 variables into console
+// console.log(date, cityName, icon, temp, humidity, speed);
 
 //function to display the info for the city
 
@@ -65,32 +76,48 @@ document.querySelector(".srchButton").addEventListener("click", handleSearch);
 //passing the city with the fetchWeather function so by default the homepage will show Atlanta's weather
 fetchWeather("Atlanta");
 
-function weatherForecast() {
-  //after changing the value of the user input, get object from local storage
+function weatherForecast(cityName) {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      cityName +
+      "&units=metric&appid=" +
+      apiKey
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
 
+//give us all 40 elements we only need every 8th element
+     let dataList = data.list;
+     let index = 1; 
+     for (let i = 0; i < dataList.length; i+=8) 
+     {
+      let tempDiv = document.getElementById(`day${index}temp`);
+      tempDiv.innerHTML=`° ${dataList[i].main.temp}`;
+      let windDiv = document.getElementById(`day${index}wind`);
+      windDiv.innerHTML = `${dataList[i].wind.speed} MPH`;
+      let humDiv = document.getElementById(`day${index}humidity`);
+      humDiv.innerHTML = `${dataList[i].main.humidity} %`;
+      index+=1
+      
+     }
+    }) ;
 
-    const weatherDetails = localStorage.getItem("searchCity");
-    weather = JSON.parse(weatherDetails)
+  //fetch another API with forecasted data
+  weatherDetails = localStorage.getItem("searchCity");
+  weather = JSON.parse(weatherDetails);
 
-    const weatherDays = [date, temp, speed];
-    const date = new Date();
-    const temp = data.list[0].main.temp;
-    const speed = data.list[0].wind.speed;
-    const humidity = data.list[0].main.humidity;
-    const icon = data.list[0].weather[0].icon;
-  };
+  //   creating an object to store inside local storage
 
-  //for loop will be executed until i is at 5 with increment of 1
-  for (let i = 0; i < 5; i++) {
-    document.querySelector("#date").innerText;
-    console.log(date[i]);
-  }
+  // for loop will be executed until i is at 5 with increment of 1
 
-  // console.log(weatherForecast);
+  console.log(weatherForecast);
+}
 
-  //storing user input
-  localStorage.setItem(searchCity, "value");
+//   //storing user input
+//   localStorage.setItem(searchCity, "value");
 
-  //retreiving previous user input
-  localStorage.getItem(searchCity, "value");
-
+//   //retreiving previous user input
+//   localStorage.getItem(searchCity, "value");
